6ef98655f2b8f1efe266dd8084966d94f76feb15
bulzaj <ernestsado94@gmail.com>
2021-10-13 21:11:19 +0200

    add account operations history route (dirty solution)

--- src/budget/account-resource.js
+++ src/budget/account-resource.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authenticate = require("../auth/authenticate");
+const { populate } = require("./accounts-model");
 const Accounts = require("./accounts-model");
+const AccountAction = require("./account-action-model");
 
 // get all user accounts
 router.get("/", authenticate, async (req, res) => {
@@ -9,5 +11,28 @@ router.get("/", authenticate, async (req, res) => {
     res.status(404).json({ message: "Accounts does not found" });
   res.status(200).json(result);
 });
+
+// get history of specyfic account
+router.get("/:id/history", authenticate, async (req, res) => {
+  const accountId = req.params.id;
+  const { accounts } = await Accounts.findOne({ owner: req.user.id });
+
+  try {
+    const account = accounts.id(accountId);
+    const operations = await fetchOperationsHistory(account.operationsHistory);
+    res.status(200).json(operations);
+  } catch (err) {
+    res.status(404).send(`Account with id ${accountId} does not exists`);
+  }
+});
+
+const fetchOperationsHistory = async (operations) => {
+  return Promise.all(
+    await operations.map(async (operation) => {
+      const result = await AccountAction.findById(operation);
+      return result;
+    })
+  );
+};
 
 module.exports = router;
